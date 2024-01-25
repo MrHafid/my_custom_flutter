2d621453e14fc01f5fb3f757df1548bd0d19bd58
Achmad Hafid Supriyadi <43329305+MrHafid@users.noreply.github.com>
2024-01-25 10:28:22 +0700

    add widget_refresh

---
+++ lib/widgets/widget_refresh.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// final GlobalKey<RefreshIndicatorState> refreshIndicatorKey =
+//     new GlobalKey<RefreshIndicatorState>();
+
+class WidgetRefresh extends StatefulWidget {
+  final bool appBar;
+  final SliverAppBar? sliverAppBar;
+  final Future Function() onRefresh;
+  final GlobalKey<RefreshIndicatorState> refreshIndicatorKey;
+  final Widget childAdapter;
+  final bool isIos;
+  const WidgetRefresh(
+      {super.key,
+      this.appBar = false,
+      this.sliverAppBar,
+      required this.refreshIndicatorKey,
+      required this.onRefresh,
+      required this.childAdapter,
+      this.isIos = true});
+
+  @override
+  _WidgetRefreshState createState() => _WidgetRefreshState();
+}
+
+class _WidgetRefreshState extends State<WidgetRefresh> {
+  @override
+  Widget build(BuildContext context) =>
+      widget.isIos ? buildIosWidget() : buildAndroidWidget();
+
+  Widget buildAndroidWidget() => RefreshIndicator(
+      key: widget.refreshIndicatorKey,
+      onRefresh: widget.onRefresh,
+      child: widget.childAdapter);
+
+  Widget buildIosWidget() => CustomScrollView(
+        physics: const BouncingScrollPhysics(
+          parent: AlwaysScrollableScrollPhysics(),
+        ),
+        slivers: widget.appBar
+            ? [
+                widget.sliverAppBar!,
+                CupertinoSliverRefreshControl(
+                  onRefresh: widget.onRefresh,
+                  key: widget.refreshIndicatorKey,
+                ),
+                SliverToBoxAdapter(
+                  child: widget.childAdapter,
+                )
+              ]
+            : [
+                CupertinoSliverRefreshControl(
+                  onRefresh: widget.onRefresh,
+                  key: widget.refreshIndicatorKey,
+                ),
+                SliverToBoxAdapter(
+                  child: widget.childAdapter,
+                )
+              ],
+      );
+}
